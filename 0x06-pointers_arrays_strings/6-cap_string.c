e24af4b4aade7a08583bca1d08fa6acecfffdee7
J-rgb-ai <opiyojoyshaney@gmail.com>
2023-07-27 07:15:30 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,35 +2,36 @@
 #include <string.h>
 /**
  * cap_string -  capitalizes all words of a string
- * @s:capitalized string
+ * @str: capitalized string
  *
- * Return: s
+ * Return: str
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	i++;
 	{
-		if (s[i] == ' ' ||
-		s[i] == '\t' || s[i] == ',' ||
-		s[i] == ';' ||
-		s[i] == '.' ||
-		s[i] == '!' ||
-		s[i] == '?' ||
-		s[i] == '"' ||
-		s[i] == '(' ||
-		s[i] == ')' ||
-		s[i] == '{' ||
-		s[i] == '}')
+		if (str[i] == ' ' ||
+		str[i] == '\t' ||
+		str[i] == ',' ||
+		str[i] == ';' ||
+		str[i] == '.' ||
+		str[i] == '!' ||
+		str[i] == '?' ||
+		str[i] == '"' ||
+		str[i] == '(' ||
+		str[i] == ')' ||
+		str[i] == '{' ||
+		str[i] == '}')
 		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				s[i + 1] = s[i + 1] - 32;
+				str[i + 1] = str[i + 1] - 32;
 			}
 		}
 	}
-	return (s);
+	return (str);
 }
