772d006d999b6a6d0b4ccc9f847d5f42ba69dfc4
J-rgb-ai <opiyojoyshaney@gmail.com>
2023-08-08 13:21:08 -0700

    to infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,32 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, sum = 0;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	int sum = 0;
-
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		int j = 0;
-
-		while (argv[i][j] != '\0')
+		if (atoi(argv[i]) < 0)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
+			printf("Error\n");
+			return (1);
 		}
-
 		sum += atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
-
 	return (0);
 }
