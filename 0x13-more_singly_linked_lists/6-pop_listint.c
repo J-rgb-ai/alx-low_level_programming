657aad7bf99f8a16bb87cdeb03a229a1fbaac97b
J-rgb-ai <opiyojoyshaney@gmail.com>
2023-08-30 01:53:23 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes head node
+ * @head: double pointer to head
+ *
+ * Return: head's node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	*head = (*head)->next;
+	n = temp->n;
+	free(temp);
+
+	return (n);
+
+	return (n);
+}
