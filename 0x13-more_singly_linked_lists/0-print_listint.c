7094314fd8a2fce11a3653a4aa76a04fc4cb6145
J-rgb-ai <opiyojoyshaney@gmail.com>
2023-08-30 00:40:55 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: pointerr that contains adress of the fist node 
+ * @h: pointer that contains adress of the fist node
  *
  * Return: number off nodes
  */
