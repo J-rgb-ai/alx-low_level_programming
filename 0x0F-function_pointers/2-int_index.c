27f1baea1abb7d6e203a5092f032f83a4c36ac0a
J-rgb-ai <opiyojoyshaney.com>
2023-10-11 12:17:51 -0700

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * int_index - checks if arry element matches
+ * @array: pointer to array of int
+ * @size: array size
+ * @cmp: pointer to a function
+ * Return: integer position
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+					return (i);
+		}
+	}
+	return (-1);
+}
