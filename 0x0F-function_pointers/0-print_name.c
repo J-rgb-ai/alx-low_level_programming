8199566e265e5e23ffd6bf147fc790112bf08a25
J-rgb-ai <opiyojoyshaney.com>
2023-10-11 11:55:27 -0700

    what is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/*
+ * print_name - prints a name
+ * @name: char pointer to a string
+ * @f: pointer function returning void
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+	(*f)(name);
+}
