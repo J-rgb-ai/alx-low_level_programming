645671770dce58e23b7d25cb079e61dc1db45901
J-rgb-ai <opiyojoyshaney.com>
2024-05-20 03:08:40 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees the list
+ * @head: head of the link list
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+}
